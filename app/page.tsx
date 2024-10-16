cb3c43d38f420232f4365e39d2618bd6429c37c9
nxvtej <nxvtej@outlook.com>
2024-10-16 17:34:08 +0530

    add goback

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,19 @@ import { SpinnerButton } from "@/components/ui/SpinnerButton";
 /** @format */
 
 import { Textarea } from "@/components/ui/textarea";
+import { useRouter } from "next/navigation";
+// import Link from "next/link";
 import { useState } from "react";
 
 export default function Home() {
-	const [prompt, setPrompt] = useState("name some cricketers");
+	const router = useRouter();
+	const [prompt, setPrompt] = useState("Name some cricketers");
 	const [response, setResponse] = useState("...");
 	const [loading, setLoading] = useState(false);
-
+	const [back, setBack] = useState(false);
+	const [placeholder, setPlaceholder] = useState(
+		"Demo Prompt: Name some cricketers"
+	);
 	const url = process.env.NEXT_PUBLIC_APIROUTE;
 	if (!url) {
 		throw new Error("URL not found");
@@ -42,26 +48,48 @@ export default function Home() {
 		}
 		setLoading(false);
 	};
+
+	const backclickHandler = async () => {
+		setBack(true);
+		setResponse("...");
+		setPlaceholder("...");
+		// redicet page to this url 100xnavi.com
+		setBack(false);
+		router.push("https://100xnavi.com");
+		// redirect("https://100xnavi.com");
+	};
 	return (
 		<div className='flex flex-col min-h-screen justify-center items-center'>
 			<div>
-				<div className='text-slate-500 text-3xl underline'>
-					<h1>Testing API</h1>
+				<div className='text-slate-500 text-3xl  underline'>
+					<h1>Testing LLama-3.0</h1>
 				</div>
 				<span className=' pt-2 text-slate-500'>Enter Prompt</span>
 				<br />
 				<div className='w-80 pt-4'>
 					<Textarea
-						placeholder='name some cricketers'
+						placeholder={placeholder}
 						className='placeholder-faded'
 						onChange={(event) => {
 							setPrompt(event.target.value);
 						}}
 					/>
 					<br />
-					<SpinnerButton name='Submit' state={loading} onClick={clickHandler}>
-						Hit BedRock
-					</SpinnerButton>
+					<div className='flex space-x-2'>
+						<SpinnerButton
+							name='Hit BedRock'
+							state={loading}
+							onClick={clickHandler}></SpinnerButton>
+						{/* <Link href='https://100xnavi.com'>
+							<SpinnerButton name='Go Back' state={back} />
+							</Link> */}
+						<SpinnerButton
+							name='Go Back'
+							state={back}
+							onClick={backclickHandler}
+						/>
+					</div>
+					{/* <SpinnerButton>Go back</SpinnerButton> */}
 				</div>
 				<br />
 				<br />
